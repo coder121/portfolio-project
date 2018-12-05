72597bfa3c7303a7c2953d2eed8dfb3f9987c10e
Siddiq Ahmed Syed <ssyed@MBP15-97HTD6-2.8x8.com>
2018-12-05 15:23:22 -0800

    added blogs

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,15 @@ class Blog(models.Model):
     title=models.CharField(max_length=200)
     pub_date=models.DateTimeField()
     image=models.ImageField(upload_to="images/")
-    body=models.CharField(max_length=200)
+    body=models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %y')
